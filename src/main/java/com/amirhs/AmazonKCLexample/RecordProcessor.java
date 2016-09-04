17f0b55e289dbfdef34b171e6affcc84ac06740b
shenavaa <shenavaa@SYD-6K3A833.ant.amazon.com>
2016-09-04 14:53:10 +1000

    Added checkpointing

--- src/main/java/com/amirhs/AmazonKCLexample/RecordProcessor.java
+++ src/main/java/com/amirhs/AmazonKCLexample/RecordProcessor.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
-
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.InvalidStateException;
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.KinesisClientLibDependencyException;
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.ShutdownException;
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.ThrottlingException;
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorCheckpointer;
 import com.amazonaws.services.kinesis.clientlibrary.interfaces.v2.IRecordProcessor;
 import com.amazonaws.services.kinesis.clientlibrary.types.InitializationInput;
 import com.amazonaws.services.kinesis.clientlibrary.types.ProcessRecordsInput;
@@ -22,13 +26,54 @@ public class RecordProcessor implements IRecordProcessor {
 	}
 
 	public void processRecords(ProcessRecordsInput records) {
+		IRecordProcessorCheckpointer checkpointer = records.getCheckpointer();
 		List<Record> lRecords = records.getRecords();
+		
 		for (Record record: lRecords) {
 			log.info(record.getApproximateArrivalTimestamp() + "\t" + record.getPartitionKey() + "\t" + record.getSequenceNumber() + "\t" + new String(record.getData().array(), Charset.forName("UTF-8")));
+			
 		}
 	}
 
 	public void shutdown(ShutdownInput arg) {
 		log.info("Shutting down RecordProcessor for shard: " + shard + " Reason: " + arg.getShutdownReason());
+		checkpoint(arg.getCheckpointer());
+	}	
+	
+	private void checkpoint (IRecordProcessorCheckpointer checkpointer,Record record) {
+		try {
+			checkpointer.checkpoint(record);
+		} catch (KinesisClientLibDependencyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ThrottlingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ShutdownException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	private void checkpoint (IRecordProcessorCheckpointer checkpointer) {
+		try {
+			checkpointer.checkpoint();
+		} catch (KinesisClientLibDependencyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ThrottlingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ShutdownException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
